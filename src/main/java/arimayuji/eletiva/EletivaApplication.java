012ed6ea1da774b52e95903f36120b9eadc5d3ca
Yuji Arima <yujiarima@hotmail.com>
2025-03-30 17:16:39 -0300

    feat: adds EletivaApplication

---
+++ src/main/java/arimayuji/eletiva/EletivaApplication.java
@@ -0,0 +1,13 @@
+package arimayuji.eletiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arimayuji/eletiva/EletivaApplicationTests.java
@@ -0,0 +1,13 @@
+package arimayuji.eletiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
