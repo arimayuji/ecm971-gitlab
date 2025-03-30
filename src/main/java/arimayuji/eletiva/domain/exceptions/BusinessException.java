e94c6aa05bb671f7ed98d3bc7d6eafdf7b2b4bec
Yuji Arima <yujiarima@hotmail.com>
2025-03-30 20:50:44 -0300

    feat(domain): adds business exception

---
+++ src/main/java/arimayuji/eletiva/domain/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package arimayuji.eletiva.domain.exceptions;
+
+public class BusinessException extends RuntimeException {
+    
+    public BusinessException(String message) {
+        super(message);
+    }
+}
