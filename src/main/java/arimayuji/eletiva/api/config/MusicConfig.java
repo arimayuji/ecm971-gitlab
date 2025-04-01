5e9e06889fed25046dd265c52bc023168a0d180b
Yuji Arima <yujiarima@hotmail.com>
2025-04-01 10:40:42 -0300

    chore(api): updates beans injection in MusicConfig class

--- src/main/java/arimayuji/eletiva/api/config/MusicConfig.java
+++ src/main/java/arimayuji/eletiva/api/config/MusicConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import arimayuji.eletiva.api.mappers.MusicDtoMapper;
 import arimayuji.eletiva.application.useacases.CreateMusicUseCase;
 import arimayuji.eletiva.application.useacases.GetAllMusicsUseCase;
+import arimayuji.eletiva.application.useacases.ReviewMusicUseCase;
 import arimayuji.eletiva.domain.gateways.MusicRepository;
 import arimayuji.eletiva.infra.repositories.jpa.JpaMusicRepository;
+import arimayuji.eletiva.infra.repositories.jpa.SpringDataMusicRepository;
 
 @Configuration
 public class MusicConfig {
@@ -23,8 +25,18 @@ public class MusicConfig {
     }
 
     @Bean
+    public ReviewMusicUseCase reviewMusicUseCase(MusicRepository musicRepository) {
+        return new ReviewMusicUseCase(musicRepository);
+    }
+
+    @Bean
     public MusicRepository musicRepository(JpaMusicRepository jpaMusicRepository) {
         return jpaMusicRepository;
+    }
+
+    @Bean
+    public JpaMusicRepository jpaMusicRepository(SpringDataMusicRepository springDataMusicRepository) {
+        return new JpaMusicRepository(springDataMusicRepository);
     }
 
     @Bean
