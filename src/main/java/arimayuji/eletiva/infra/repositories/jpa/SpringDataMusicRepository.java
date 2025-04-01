80eeee49567a90fe24e4a0cb6ad3badad509a37f
Yuji Arima <yujiarima@hotmail.com>
2025-04-01 10:37:21 -0300

    chore(infra): adds new method in SpringDataRepository

--- src/main/java/arimayuji/eletiva/infra/repositories/jpa/SpringDataMusicRepository.java
+++ src/main/java/arimayuji/eletiva/infra/repositories/jpa/SpringDataMusicRepository.java
@@ -1,14 +1,16 @@
 package arimayuji.eletiva.infra.repositories.jpa;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import arimayuji.eletiva.infra.entities.JpaMusic;
 
-public interface SpringDataMusicRepository extends JpaRepository<JpaMusic, UUID> {
+public interface SpringDataMusicRepository extends JpaRepository<JpaMusic, Long> {
 
     Optional<JpaMusic> findByName(String name);
+
+    List<JpaMusic> findAllByOrderByReviewDescNameAsc();
 
 }
